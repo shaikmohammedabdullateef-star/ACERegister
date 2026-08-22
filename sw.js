b3b00b027328d646520066a717d8c52a604c5fc9
shaikmohammedabdullateef-star <abdullateef216786@gmail.com>
2026-08-22 21:03:26 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ace-register-v1";
+const CACHE = "ace-register-v2";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./firebase-config.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-small.png"];
 
 self.addEventListener("install", (e) => {
@@ -15,6 +15,12 @@ self.addEventListener("activate", (e) => {
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match("./index.html")))
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((cached) => cached || caches.match("./index.html")))
   );
 });
